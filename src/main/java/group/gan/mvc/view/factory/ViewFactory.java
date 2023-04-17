1632b9319907d49ed57110815defdfa3585b985f
San_Check <2162381070@qq.com>
2023-04-17 22:18:31 +1000

    Complete ViewFactory Interface Design

--- src/main/java/group/gan/mvc/view/factory/ViewFactory.java
+++ src/main/java/group/gan/mvc/view/factory/ViewFactory.java
@@ -1,4 +1,14 @@
 package group.gan.mvc.view.factory;
 
+import group.gan.mvc.view.View;
+
+/**
+ * A View factory interface for implementing the factory pattern
+ */
 public interface ViewFactory {
+    /**
+     * A function for generating View
+     * @return The specific implementation class of View
+     */
+    View createView();
 }
