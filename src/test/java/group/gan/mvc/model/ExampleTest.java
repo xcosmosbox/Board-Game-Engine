25cbe9c09432994518eb84e3f10a07b95bb59681
San_Check <2162381070@qq.com>
2023-04-17 17:08:05 +1000

    upload Example Test

---
+++ src/test/java/group/gan/mvc/model/ExampleTest.java
@@ -0,0 +1,19 @@
+package group.gan.mvc.model;
+
+import org.junit.Test;
+
+/**
+ * @author: fengyuxiang
+ * @ClassName: ExampleTest
+ * @version: 1.0
+ * @description:
+ * @create: 17/4/2023
+ */
+
+public class ExampleTest {
+
+    @Test
+    public void TestExample(){
+
+    }
+}
