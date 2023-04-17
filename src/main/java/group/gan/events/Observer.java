8898c449c8962d17ae5591a2a565c302d8713bd0
San_Check <2162381070@qq.com>
2023-04-17 22:45:57 +1000

    Complete events Interface Design

--- src/main/java/group/gan/events/Observer.java
+++ src/main/java/group/gan/events/Observer.java
@@ -1,4 +1,12 @@
 package group.gan.events;
 
+/**
+ * Interface for implementing the observer pattern
+ */
 public interface Observer {
+    /**
+     * Observer interface for receiving information
+     * @param observable
+     */
+    void notifyObserver(Observable observable);
 }
