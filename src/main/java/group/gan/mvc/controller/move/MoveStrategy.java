d31686791bc19cfba179ac8bc3329fb853222a3b
San_Check <2162381070@qq.com>
2023-04-17 22:20:30 +1000

    Complete MoveStrategy Interface Design

--- src/main/java/group/gan/mvc/controller/move/MoveStrategy.java
+++ src/main/java/group/gan/mvc/controller/move/MoveStrategy.java
@@ -1,4 +1,17 @@
 package group.gan.mvc.controller.move;
 
+import group.gan.mvc.controller.board.Board;
+import group.gan.mvc.controller.player.Player;
+
+/**
+ * Interface for implementing the strategy pattern
+ */
 public interface MoveStrategy {
+    /**
+     * Run a specific strategy
+     * @param player one player object reference
+     * @param board one board object reference
+     * @return Criteria for successful operation
+     */
+    Boolean executeStrategy(Player player, Board board);
 }
