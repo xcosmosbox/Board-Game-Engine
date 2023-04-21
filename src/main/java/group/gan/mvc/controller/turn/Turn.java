af3d87163bc116cbf196e140576997782c70b3d9
Charles Wang <s3894453@student.rmit.edu.au>
2023-04-22 01:58:45 +1000

    third update: just put all the method possible, will delete some later

--- src/main/java/group/gan/mvc/controller/turn/Turn.java
+++ src/main/java/group/gan/mvc/controller/turn/Turn.java
@@ -6,6 +6,10 @@ import group.gan.mvc.controller.player.Player;
 import group.gan.mvc.model.coordinate.Coordinate;
 import group.gan.mvc.model.token.Token;
 
+//the aim of this interface is: Turn is used to control the round in the game. Turn stores the player. Whenever the  Game runs a Turn, the Turn will notify the player to play with the game. After a  player has played a turn, the Turn changes who will be playing the next time.
+
+
+
 /**
  * Turn controller.
  *
