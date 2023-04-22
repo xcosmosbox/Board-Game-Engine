6c5a585d16def799736f27cca02d9c396d010a08
San_Check <2162381070@qq.com>
2023-04-22 16:14:32 +1000

    Complete the PositionImpl

---
+++ src/main/java/group/gan/mvc/model/position/impl/PositionImpl.java
@@ -0,0 +1,63 @@
+package group.gan.mvc.model.position.impl;
+
+import group.gan.mvc.model.position.Position;
+import group.gan.mvc.model.token.Token;
+
+import java.util.Stack;
+
+/**
+ * @author: fengyuxiang
+ * @ClassName: PositionImpl
+ * @version: 1.0
+ * @description:
+ * @create: 22/4/2023
+ */
+public class PositionImpl implements Position {
+
+    /**
+     * private method
+     * using stack data structure to store token
+     * maximum capacity = 1
+     */
+    private Stack<Token> token = new Stack<>();
+
+    /**
+     * Get the Token stored on the Position, but not remove it from the Position
+     *
+     * @return Token
+     */
+    @Override
+    public Token peekToken() {
+        return token.peek();
+    }
+
+    /**
+     * Pass a token to store on the position
+     *
+     * @param token a token object
+     */
+    @Override
+    public void addToken(Token token) {
+        this.token.push(token);
+    }
+
+    /**
+     * Get the Token stored on the Position, and remove it from the Position
+     *
+     * @return Token
+     */
+    @Override
+    public Token removeToken() {
+        return token.pop();
+    }
+
+    /**
+     * Check if there is a Token on the Position
+     *
+     * @return Boolean
+     */
+    @Override
+    public Boolean isEmpty() {
+        return token.isEmpty();
+    }
+}
