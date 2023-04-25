c452786f7da03bd863dbadb8a2c1d21ba6b3bcfc
LOLgorithm <tliuandrew@gmail.com>
2023-04-25 17:36:15 +1000

    GameStateImpl finished.

--- src/main/java/group/gan/mvc/model/game/state/impl/GameStateImpl.java
+++ src/main/java/group/gan/mvc/model/game/state/impl/GameStateImpl.java
@@ -1,10 +1,37 @@
 package group.gan.mvc.model.game.state.impl;
 
 import group.gan.mvc.model.game.state.GameState;
+import group.gan.mvc.model.game.state.GameStateEnum;
+
+/**
+ * @author Tianyi Liu
+ * @className GameStateImpl
+ * @version 1.0
+ * @created 25/4/2023
+ */
 
 public class GameStateImpl implements GameState {
+    /**
+     * a private variable which stores game state enum
+     */
+    private GameStateEnum gameStateEnum;
+
+    /**
+     * no-parameter constructor
+     */
+    public GameStateImpl() {
+        this.gameStateEnum = GameStateEnum.RUN;
+    }
+
+    /**
+     * Check if the game is over
+     *
+     * @return boolean value indicating if the game is over
+     */
     @Override
     public Boolean isGameOver() {
-        return null;
+        return gameStateEnum != GameStateEnum.RUN;
     }
+
+
 }
