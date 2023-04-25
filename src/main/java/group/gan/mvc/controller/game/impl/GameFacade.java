a63ba414abc6cf78dc6e5219f95c9b57073e6c19
LOLgorithm <tliuandrew@gmail.com>
2023-04-25 17:35:04 +1000

    Added 3 classes, GameFacade in the controller package which implements Game, GameModelImpl in the model package which implements GameModel, GameStateImpl which is also in the model package implements GameState. GameModelImpl is almost finished.

---
+++ src/main/java/group/gan/mvc/controller/game/impl/GameFacade.java
@@ -0,0 +1,26 @@
+package group.gan.mvc.controller.game.impl;
+
+import group.gan.mvc.controller.game.Game;
+import group.gan.mvc.model.game.GameModel;
+
+public class GameFacade implements Game {
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void quit() {
+
+    }
+
+    @Override
+    public void quitForWin() {
+
+    }
+
+    @Override
+    public void build(GameModel gameModel) {
+
+    }
+}
---
+++ src/main/java/group/gan/mvc/model/game/state/impl/GameStateImpl.java
@@ -0,0 +1,10 @@
+package group.gan.mvc.model.game.state.impl;
+
+import group.gan.mvc.model.game.state.GameState;
+
+public class GameStateImpl implements GameState {
+    @Override
+    public Boolean isGameOver() {
+        return null;
+    }
+}
