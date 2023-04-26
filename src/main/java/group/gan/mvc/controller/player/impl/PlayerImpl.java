d0a0e4798e2839da81326be7602864ba4e4e1bab
Charles Wang <s3894453@student.rmit.edu.au>
2023-04-26 19:15:51 +1000

    UPDATE: add comment to all the method in the PlayerImpl

--- src/main/java/group/gan/mvc/controller/player/impl/PlayerImpl.java
+++ src/main/java/group/gan/mvc/controller/player/impl/PlayerImpl.java
@@ -21,32 +21,41 @@ import java.util.Scanner;
 
 public class PlayerImpl implements Player, Pollable {
 
+    // Declare a PlayerModel instance
     private PlayerModel playerModel;
 
+    // Constructor for the PlayerImpl class, taking a PlayerModel as an argument
     public PlayerImpl(PlayerModel playerModel) {
+        // Assign the provided playerModel to the class variable
         this.playerModel = playerModel;
     }
 
+    // just add this for the further use
     @Override
     public Command play() {
         return null;
     }
 
+    // Injection interface for setting PlayerModel
     @Override
     public void setStateManager(PlayerModel playerModel) {
-        // a injection interface for player model
+        // an injection interface for player model
+        this.playerModel = playerModel;
     }
 
+    // Getter for the player's unique ID
     @Override
     public Integer getUid() {
         return playerModel.getUid();
     }
 
+    // Getter for one of the player's tokens
     @Override
     public Token getOneToken() {
         return playerModel.getOneToken();
     }
 
+    // Getter for the player's name
     @Override
     public String getPlayerName() {
         return playerModel.getPlayerName();
@@ -55,13 +64,14 @@ public class PlayerImpl implements Player, Pollable {
     /**
      * get player state
      *
-     * @return
+     * @return the player's current state
      */
     @Override
     public PlayerStateEnum getPlayerState() {
         return playerModel.getState();
     }
 
+    // Request a single integer input from the user
     @Override
     public Integer requestOneIntegerInput() {
         Scanner singleInput = new Scanner(System.in);
@@ -69,13 +79,16 @@ public class PlayerImpl implements Player, Pollable {
         return input;
     }
 
+    // Request multiple integer inputs from the user
     @Override
     public Integer[] requestIntegersInput() {
         Scanner multipleInput = new Scanner(System.in);
         String input = multipleInput.nextLine();
 
+        // Split the input string into an array
         String[] inputArray = input.split(",");
 
+        // Convert the string array into an Integer array
         Integer[] intArray = new Integer[inputArray.length];
 
         try {
@@ -89,23 +102,32 @@ public class PlayerImpl implements Player, Pollable {
 
     }
 
+    // Poll for user input with a provided View
     @Override
     public CommandType poll(View view) {
+        // get the text message that will be displayed
         Display display = new Display();
         view.draw(display);
+        //get the user input
         int input = requestOneIntegerInput();
         return CommandTypeConverter.IntegerToCommandType(input, view);
 
     }
 
+    // Fill in the necessary information for a command
     @Override
     public Command fillCommand(Command command) {
+        // If the command is of type MOVE
         if (command.getCommandType() == CommandType.MOVE) {
+
 //            MoveCommand moveCommand = (MoveCommand) command;
 //            moveCommand.init(this);
+
+            // Cast the command to MoveCommand and initialize it with the player
             ((MoveCommand) command).init(this);
             MoveStrategyFactory moveStrategyFactory = null;
 
+            // Create a move strategy depending on the player's current state
             if (this.playerModel.getState() == PlayerStateEnum.PLACING){
                 moveStrategyFactory = new PlacingStrategyFactory();
 //                moveCommand.initMoveStrategy(moveStrategyFactory.createStrategy());
@@ -126,6 +148,7 @@ public class PlayerImpl implements Player, Pollable {
         return command;
     }
 
+    // Override the equals method for comparing PlayerImpl objects
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -134,15 +157,20 @@ public class PlayerImpl implements Player, Pollable {
         return Objects.equals(playerModel, player.playerModel);
     }
 
+    // Override the hashCode method for PlayerImpl objects
     @Override
     public int hashCode() {
         return Objects.hash(playerModel);
     }
 
+    // Override the toString method for the PlayerImpl class
     @Override
     public String toString() {
+        // Get all tokens of the player
         Token[] allTokens = playerModel.getTokens();
+        // Get the symbol of the first token
         String symbol = String.valueOf(allTokens[0].getSymbol());
+        // Return a string representation of the player with their name and token color
         return getPlayerName() + " / Token Color: " + symbol;
     }
 }
