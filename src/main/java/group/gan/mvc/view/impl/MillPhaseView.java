766556dc20ea4053fea56160e65839ba9682b618
Charles Wang <s3894453@student.rmit.edu.au>
2023-05-13 22:57:38 +1000

    Update: Create MillPhaseView

---
+++ src/main/java/group/gan/mvc/view/impl/MillPhaseView.java
@@ -0,0 +1,11 @@
+package group.gan.mvc.view.impl;
+
+import group.gan.mvc.view.View;
+import group.gan.utils.Display;
+
+public class MillPhaseView implements View {
+    @Override
+    public void draw(Display display) {
+
+    }
+}
